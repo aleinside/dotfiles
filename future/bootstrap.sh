b282568208b10767d79c3cc12d2d78756170104b
Alessandro Nuzzo <aleinside@gmail.com>
2018-01-17 22:30:38 +0100

    locale-gen

--- future/bootstrap.sh
+++ future/bootstrap.sh
@@ -10,6 +10,8 @@ sudo apt-get update
 sudo apt-get install -y awscli libcurl4-openssl-dev cmake pkg-config 
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
+sudo locale-gen it_IT.UTF-8
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
