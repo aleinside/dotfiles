f9642a81705d99769c4734790c8161564e0402fb
Alessandro Nuzzo <aleinside@gmail.com>
2018-01-25 17:52:57 +0100

    update ip

--- bin/update-ip.sh
+++ bin/update-ip.sh
@@ -3,5 +3,9 @@
 IP=$1
 HOST="prima-dev"
 
-sed "/${HOST}/ s/.*/${IP}	${HOST}/g" /etc/hosts | sudo tee /etc/hosts
-sed "/HostName/ s/.*/HostName ${IP}/g" ~/.ssh/config > ~/.ssh/config
+sed "/${HOST}/ s/.*/${IP}	${HOST}/g" /etc/hosts > /tmp/hostsBK
+cat /tmp/hostsBK | sudo tee /etc/hosts
+rm /tmp/hostsBK
+sed "/HostName/ s/.*/HostName ${IP}/g" ~/.ssh/config > /tmp/sshconfig
+cat /tmp/sshconfig > ~/.ssh/config
+rm /tmp/sshconfig
