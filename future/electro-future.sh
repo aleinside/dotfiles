48109217f518df9325eb88d4d8e0a709132ef1f3
Alessandro Nuzzo <aleinside@gmail.com>
2018-02-01 23:01:51 +0100

    colori

--- future/electro-future.sh
+++ future/electro-future.sh
@@ -2,27 +2,49 @@
 #set -e
 #set -x
 
-# configurazione cartelle e host
-# notifica https://apple.stackexchange.com/questions/57412/how-can-i-trigger-a-notification-center-notification-from-an-applescript-or-shel<Paste>
-
 ### --- ###
 # .ssh/config
 # export ELECTRO_INSTANCE_ID
 ### --- ###
 
-### da configurare ###
-HOST="prima-dev"
-SSH_HOST="dev-future"
-DIR_PROJECT=~/Works/prima
-REMOTE_PATH="/home/ubuntu"
-REMINDER_TIME="1750"
-### -------------- ###
-
-LOG_PATH="/tmp/my-sync.log"
+CONFIG_PATH=~/.config/electro/
+CONFIG_FILE="config"
+SCRIPT_URL="https://raw.githubusercontent.com/aleinside/dotfiles/master/future/electro-future.sh"
+LOG_PATH="/tmp/electro-sync.log"
 
 INSTANCE_STATUS_CMD="aws ec2 describe-instance-status --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep SYSTEMSTATUS | awk '{print \$2}'"
 FSWATCH_CMD="fswatch -o ${DIR_PROJECT} | my_rsync ${DIR_PROJECT} ${SSH_HOST} ${REMOTE_PATH} &"
 INSTANCE_DESCRIBE_STATUS_CMD="aws ec2 describe-instances --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep -w STATE |awk '{print \$3}'"
+
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+
+purple=$(tput setaf 171)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+tan=$(tput setaf 3)
+blue=$(tput setaf 38)
+
+e_header() {
+  printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@"
+}
+
+e_arrow() {
+  printf "➜ $@\n"
+}
+
+e_success() {
+  printf "${green}✔ %s${reset}\n" "$@"
+}
+
+e_error() {
+  printf "${red}✖ %s${reset}\n" "$@"
+}
+
+e_warning() {
+  printf "${tan}➜ %s${reset}\n" "$@"
+}
 
 notify() {
     osascript -e "display notification \"$*\" with title \"Electro Future\""
@@ -51,7 +73,7 @@ my_rsync() {
 }
 
 start_services() {
-    printf "Inizio avviando l'istanza ${ELECTRO_INSTANCE_ID}\n"
+    e_header "Inizio avviando l'istanza ${ELECTRO_INSTANCE_ID}"
 
     aws ec2 start-instances --instance-ids ${ELECTRO_INSTANCE_ID} > /dev/null
 
@@ -61,16 +83,17 @@ start_services() {
     do
         INSTANCE_STATUS=$(eval $INSTANCE_STATUS_CMD)
         sleep 2
-        printf "."
+        echo "."
     done
 
-    printf "\nIstanza pronta\n"
+    e_success "Istanza pronta"
 
     #IP_KEY="PRIVATEIPADDRESS"
     IP_KEY="ASSOCIATION"
     INSTANCE_IP=$(aws ec2 describe-instances --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep ${IP_KEY} |head -1 |awk '{print $4}')
 
-    printf "Ottenuto IP ${INSTANCE_IP}: aggiorno ~/.ssh/config e /etc/hosts\n"
+    e_success "Ottenuto IP ${INSTANCE_IP}"
+	e_warning "Aggiorno ~/.ssh/config e /etc/hosts"
 
     sed "/${HOST}/ s/.*/${INSTANCE_IP}	${HOST}/g" /etc/hosts > /tmp/hostsBK
     cat /tmp/hostsBK | sudo tee /etc/hosts 1> /dev/null
@@ -79,68 +102,103 @@ start_services() {
     cat /tmp/sshconfig > ~/.ssh/config
     rm /tmp/sshconfig
 
-    printf "Inizializzo fswatch: log su ${LOG_PATH}\n"
+    e_warning "Inizializzo fswatch: log su ${LOG_PATH}"
     #fswatch -o ${DIR_PROJECT} | my_rsync ${DIR_PROJECT} ${SSH_HOST} ${REMOTE_PATH} &
     $(eval $FSWATCH_CMD) & export ELECTRO_FSWATCH_PID=$!
-    printf "(PID per fswatch: ${ELECTRO_FSWATCH_PID})\n"
-    printf "Puoi connetterti via ssh con ssh ${SSH_HOST}\n"
+    e_success "(PID per fswatch: ${ELECTRO_FSWATCH_PID})"
+    e_success "Puoi connetterti via ssh con ssh ${SSH_HOST}"
 
     notification_for_mac "Ricordati di spegnere la macchina remota!" &
 }
 
 stop_services() {
-    printf "Stoppo l'istanza\n"
+	e_warning "Stoppo l'istanza"
     aws ec2 stop-instances --instance-ids ${ELECTRO_INSTANCE_ID} > /dev/null
     sleep 5
-    printf "Status: $(eval $INSTANCE_DESCRIBE_STATUS_CMD)\n"
-    printf "Killo fswatch\n"
+    e_warning "Status: $(eval $INSTANCE_DESCRIBE_STATUS_CMD)"
+    e_warning "Killo fswatch"
     kill -9 ${ELECTRO_FSWATCH_PID}
 }
 
 check() {
     local INSTANCE_STATUS=$(eval $INSTANCE_DESCRIBE_STATUS_CMD)
-    printf "Status dell'istanza: ${INSTANCE_STATUS}\n"
+	if [[ ${INSTANCE_STATUS} != "ok" ]];then
+		e_error "Stato dell'istanza: ${INSTANCE_STATUS}"
+	else
+		e_success "Stato dell'istanza: ok"
+	fi
     ps cax |grep fswatch > /dev/null
     if [ $? -eq 0 ]; then
-        printf "fswatch è attivo\n"
+        e_success "fswatch è attivo"
     else
-        printf "fswatch non è attivo\n"
+        e_error "fswatch non è attivo"
     fi
 }
 
 rewatch() {
-    printf "Rilancio fswatch\n"
+    e_warning "Rilancio fswatch"
     $(eval $FSWATCH_CMD) & export ELECTRO_FSWATCH_PID=$!
-    printf "(PID per fswatch: ${ELECTRO_FSWATCH_PID})\n"
+    e_success "(PID per fswatch: ${ELECTRO_FSWATCH_PID})"
 }
 
 logwatch() {
-    printf "Log di fswatch:\n\n"
+    e_header "Log di fswatch"
     tail -f ${LOG_PATH}
 }
 
+update() {
+    local SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+    local SCRIPTNAME=$(basename "$0")
+    e_header "Procedo con l'aggiornamento dello script"
+    e_arrow "Salvo in ${SCRIPTPATH}/${SCRIPTNAME}"
+    curl -fsSL ${SCRIPT_URL} -o ${SCRIPTPATH}/${SCRIPTNAME}
+	if [ $? -eq 0 ]; then
+		e_success "Aggiornamento riuscito!"
+    else
+		e_error "Problemi nell'aggiornamento"
+    fi
+}
+
 usage() {
-    printf "Utilizzo ${0}:\n"
-    printf "   start: avvia la macchina e fswatch\n"
-    printf "   stop: stoppa fswatch e la macchina\n"
-    printf "   check: controlla lo stato della macchina e di fswatch\n"
-    printf "   rewatch: rilancia fswatch\n"
-    printf "   logwatch: controlla i log di fswatch\n"
-    printf "   test-notification: testa le notifiche alle ${REMINDER_TIME}\n\n"
+    e_header "Utilizzo $(basename $0)"
+    e_arrow "start:\t\t avvia la macchina e fswatch"
+    e_arrow "stop:\t\t\t stoppa fswatch e la macchina"
+    e_arrow "check:\t\t controlla lo stato della macchina e di fswatch"
+    e_arrow "rewatch:\t\t rilancia fswatch"
+    e_arrow "logwatch:\t\t controlla i log di fswatch"
+    e_arrow "update:\t\t aggiorna lo script"
+    e_arrow "test-notification:\t testa le notifiche alle ${REMINDER_TIME}"
 }
 
 main() {
     local cmd=$1
 
-    if [ -z ${ELECTRO_INSTANCE_ID+x} ]; then
-        printf "Per favore, configura la variabile ELECTRO_INSTANCE_ID\n"
+    if [ ! -f ${CONFIG_PATH}${CONFIG_FILE} ]; then
+        e_warning "Creo file di configurazione in ${CONFIG_PATH}"
+        mkdir -p ${CONFIG_PATH}
+        touch ${CONFIG_PATH}${CONFIG_FILE}
+        echo 'ELECTRO_INSTANCE_ID="i-xxx"
+HOST="prima-dev"
+SSH_HOST="dev-prima"
+DIR_PROJECT=~/Documents/prima
+REMOTE_PATH="/home/ubuntu"
+REMINDER_TIME="1750"
+' > ${CONFIG_PATH}${CONFIG_FILE}
+
+        e_warning "Ricordati di configurare il file!"
         exit
     fi
+    #if [ -z ${ELECTRO_INSTANCE_ID+x} ]; then
+    #    printf "Per favore, configura la variabile ELECTRO_INSTANCE_ID\n"
+    #    exit
+    #fi
 
     if [[ -z "$cmd" ]]; then
         usage
         exit 1
     fi
+
+    source ${CONFIG_PATH}${CONFIG_FILE}
 
     if [[ $cmd == "start" ]]; then
         start_services
@@ -154,6 +212,8 @@ main() {
         logwatch
     elif [[ $cmd == "test-notification" ]]; then
         notify "Test notification" &
+    elif [[ $cmd == "update" ]]; then
+        update
     else
         usage
     fi
