1e660ad25f33271825c6bd985b4bdcb1443031a0
Alessandro Nuzzo <aleinside@gmail.com>
2018-02-22 15:16:46 +0100

    new version

--- future/electro-future.sh
+++ future/electro-future.sh
@@ -1,56 +1,48 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-ELECTRO_DEBUG="0"
-if [[ "1" == $ELECTRO_DEBUG ]]; then
-    set -x
-fi
 
-# configuration vabiables, read from config file
+# TODO: watch list: visualizza lo stato di fswatch
+
+# configuration variables, read from config file
 ELECTRO_INSTANCE_ID="i-xxx"
 HOST="prima"
 SSH_HOST="dev-prima"
-DIR_PROJECT=""
-# opzionale
+# opzionali
 REMOTE_PATH="/home/ubuntu"
-# opzionale
 REMINDER_TIME="1750"
 
-
-PING_VPN="10.33.4.8"
-CONFIG_PATH=~/.config/electro/
-CONFIG_FILE="config"
-SCRIPT_URL="https://raw.githubusercontent.com/aleinside/dotfiles/master/future/electro-future.sh"
-LOG_PATH="/tmp/electro-sync.log"
-
-bold=$(tput bold)
-underline=$(tput sgr 0 1)
-reset=$(tput sgr0)
-
-purple=$(tput setaf 171)
-red=$(tput setaf 1)
-green=$(tput setaf 76)
-tan=$(tput setaf 3)
-blue=$(tput setaf 38)
+readonly ping_vpn="10.33.4.8"
+readonly config_path=~/.config/electro/
+readonly config_file="config"
+readonly script_url="https://raw.githubusercontent.com/aleinside/dotfiles/master/future/electro-future.sh"
+readonly log_path="/tmp/electro-log/"
+readonly rsync_options="--exclude=.git/ --exclude=vendor/ --exclude=node_modules/ --exclude=web/assets/ --exclude=web/bower/ --exclude=web/compass/ --exclude=web/bundles/ --exclude=web/webpack/ --exclude=var/cache/ --exclude=var/logs/ --exclude=var/sessions/ --exclude=elm-stuff/ --exclude=app/data/ --exclude=_build/ --exclude=deps/ "
+readonly bold=$(tput bold)
+readonly reset=$(tput sgr0)
+readonly purple=$(tput setaf 171)
+readonly red=$(tput setaf 1)
+readonly green=$(tput setaf 76)
+readonly tan=$(tput setaf 3)
 
 e_header() {
-    printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@"
+    printf "\\n${bold}${purple}==========  %s  ==========${reset}\\n" "$@"
 }
 
 e_arrow() {
-    printf "➜ $@\n"
+    printf "➜ %s\\n" "$@"
 }
 
 e_success() {
-    printf "${green}✔ %s${reset}\n" "$@"
+    printf "${green}✔ %s${reset}\\n" "$@"
 }
 
 e_error() {
-    printf "${red}✖ %s${reset}\n" "$@"
+    printf "${red}✖ %s${reset}\\n" "$@"
 }
 
 e_warning() {
-    printf "${tan}➜ %s${reset}\n" "$@"
+    printf "${tan}➜ %s${reset}\\n" "$@"
 }
 
 notify() {
@@ -58,46 +50,113 @@ notify() {
 }
 
 notification_for_mac() {
-    local TIME="$(date +'%H%M')"
-    if [ ${TIME} != ${REMINDER_TIME} ] ; then
+    local time
+    local check_os
+    time="$(date +'%H%M')"
+    if [ "${time}" != "${REMINDER_TIME}" ] ; then
         sleep 50
-        notification_for_mac $* &
+        notification_for_mac "$*" &
     else
-        local CHECK_OS=$(uname -s)
-        if [[ ${CHECK_OS} == "Darwin" ]]; then
-            notify $*
+        check_os=$(uname -s)
+        if [[ ${check_os} == "Darwin" ]]; then
+            notify "$*"
         else
-            printf "\t === Mi spiace, ma la notifica bella non funziona: $* === \n"
+            printf "\\t === Mi spiace, ma la notifica bella non funziona: %s === \\n" "$*"
         fi
     fi
 }
 
 check_vpn() {
     set +e
-    ping -c2 ${PING_VPN} > /dev/null
-    if [ ! $? -eq 0 ]; then
+    if ! ping -c2 -q "${ping_vpn}" > /dev/null; then
         e_warning "Non hai attivato la VPN!"
         exit
     fi
     set -e
+    true
+}
+
+get_project() {
+    basename "$(pwd)"
 }
 
 kill_fswatch() {
     set +e
-    # local status=$?
-    # if $(exit status); then
-    ps cax |grep fswatch > /dev/null
-    if [ $? -eq 0 ]; then
-        kill $(ps cax |grep fswatch |awk '{print $1}')
-    fi
+    fswatch_pid=$(cat "${log_path}$(get_project).pid")
+    kill -9 "${fswatch_pid}"
     set -e
+}
+
+start_services() {
+    local check_os
+    local instance_status
+    local oldip
+    local ip_key
+    local instance_ip
+    local k
+
+    e_header "Inizio avviando l'istanza ${ELECTRO_INSTANCE_ID}"
+
+    check_vpn
+
+    check_os=$(uname -s)
+    if [[ ${check_os} == "Darwin" ]]; then
+        ssh-add -K ~/.ssh/id_rsa
+    else
+        ssh-add ~/.ssh/id_rsa
+    fi
+
+    instance_status=$(eval "${instance_describe_status_cmd}")
+    if [[ "${instance_status}" == "stopped" ]]; then
+        aws ec2 start-instances --instance-ids "${ELECTRO_INSTANCE_ID}" > /dev/null
+        # bisogna slippare
+        for k in $(seq 1 5);
+        do
+            printf "."
+            sleep 2
+        done
+        while [[ "${instance_status}" != "ok" ]]
+        do
+            instance_status=$(eval "${instance_status_cmd}")
+            sleep 2
+            printf "."
+        done
+    fi
+    printf "\\n"
+
+    e_success "Istanza pronta"
+
+    ip_key="PRIVATEIPADDRESS"
+    #ip_key="ASSOCIATION"
+    instance_ip=$(aws ec2 describe-instances --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep ${ip_key} |head -1 |awk '{print $4}')
+
+    e_success "Ottenuto IP ${instance_ip}"
+    e_warning "Aggiorno ~/.ssh/config e /etc/hosts"
+
+    sed "/${HOST}/ s/.*/${instance_ip}	${HOST}/g" /etc/hosts > /tmp/hostsBK
+    cat /tmp/hostsBK | sudo tee /etc/hosts 1> /dev/null
+    rm /tmp/hostsBK
+    #sed "/HostName/ s/.*/HostName ${INSTANCE_IP}/g" ~/.ssh/config > /tmp/sshconfig
+    oldip=$(grep -w ${SSH_HOST} -A 1 ~/.ssh/config | awk '/HostName/ {print $2}')
+    sed "s/${oldip}/${instance_ip}/g" ~/.ssh/config > /tmp/sshconfig
+    cat /tmp/sshconfig > ~/.ssh/config
+    rm /tmp/sshconfig
+
+    if should_start_fswatch "$@"; then
+        watch
+    fi
+
+    e_success "Ora puoi lanciare electro watch nelle cartelle dei progetti"
+    e_success "Ricorda: puoi connetterti via ssh con ssh ${SSH_HOST}"
+
+    notification_for_mac "Ricordati di spegnere la macchina remota!" &
 }
 
 should_start_fswatch() {
     local status=true
     for var in "$@"
     do
-        if [ $var == "--nowatch" ]; then
+        if [ "${var}" == "--nowatch" ]; then
             status=false
             break
         fi
@@ -105,80 +164,32 @@ should_start_fswatch() {
     ${status}
 }
 
-start_services() {
-    e_header "Inizio avviando l'istanza ${ELECTRO_INSTANCE_ID}"
-
-    check_vpn
-
-    local CHECK_OS=$(uname -s)
-    if [[ ${CHECK_OS} == "Darwin" ]]; then
-        ssh-add -K ~/.ssh/id_rsa
-    else
-        ssh-add ~/.ssh/id_rsa
-    fi
-
-    local INSTANCE_STATUS=$(eval $INSTANCE_DESCRIBE_STATUS_CMD)
-    if [[ ${INSTANCE_STATUS} == "stopped" ]]; then
-        aws ec2 start-instances --instance-ids ${ELECTRO_INSTANCE_ID} > /dev/null
-        while [ "${INSTANCE_STATUS}" != "ok" ]
-        do
-            INSTANCE_STATUS=$(eval $INSTANCE_STATUS_CMD)
-            sleep 2
-            printf "."
-        done
-    fi
-
-    e_success "Istanza pronta"
-
-    IP_KEY="PRIVATEIPADDRESS"
-    #IP_KEY="ASSOCIATION"
-    INSTANCE_IP=$(aws ec2 describe-instances --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep ${IP_KEY} |head -1 |awk '{print $4}')
-
-    e_success "Ottenuto IP ${INSTANCE_IP}"
-    e_warning "Aggiorno ~/.ssh/config e /etc/hosts"
-
-    sed "/${HOST}/ s/.*/${INSTANCE_IP}	${HOST}/g" /etc/hosts > /tmp/hostsBK
-    cat /tmp/hostsBK | sudo tee /etc/hosts 1> /dev/null
-    rm /tmp/hostsBK
-    #sed "/HostName/ s/.*/HostName ${INSTANCE_IP}/g" ~/.ssh/config > /tmp/sshconfig
-    local OLDIP=$(grep -w ${SSH_HOST} -A 1 ~/.ssh/config | awk '/HostName/ {print $2}')
-    sed "s/${OLDIP}/${INSTANCE_IP}/g" ~/.ssh/config > /tmp/sshconfig
-    cat /tmp/sshconfig > ~/.ssh/config
-    rm /tmp/sshconfig
-
-    if should_start_fswatch "$@"; then
-        e_warning "Inizializzo fswatch: log su ${LOG_PATH}"
-        $(eval $FSWATCH_CMD) &
-        if [ $? -eq 0 ]; then
-            e_success "Fswatch lanciato"
-        else
-            e_error "Problemi con fswatch"
-        fi
-    fi
-    e_success "Puoi connetterti via ssh con ssh ${SSH_HOST}"
-
-    notification_for_mac "Ricordati di spegnere la macchina remota!" &
-}
-
 stop_services() {
+    local instance_status
     e_warning "Stoppo l'istanza"
     aws ec2 stop-instances --instance-ids ${ELECTRO_INSTANCE_ID} > /dev/null
     sleep 5
-    e_warning "Status: $(eval $INSTANCE_DESCRIBE_STATUS_CMD)"
+    instance_status=$(eval "${instance_describe_status_cmd}")
+    e_warning "Status: ${instance_status}"
     e_warning "Killo fswatch"
-    kill_fswatch
+    killall_fswatch
 }
 
 check() {
-    local INSTANCE_STATUS=$(eval $INSTANCE_DESCRIBE_STATUS_CMD)
-    if [[ ${INSTANCE_STATUS} != "running" ]]; then
-        e_error "Stato dell'istanza: ${INSTANCE_STATUS}"
+    local instance_status
+    local fswatch_pid
+    local pid_file
+    check_vpn
+    instance_status=$(eval "${instance_describe_status_cmd}")
+    if [[ "${instance_status}" != "running" ]]; then
+        e_error "Stato dell'istanza: ${instance_status}"
     else
         e_success "Stato dell'istanza: ok"
     fi
     set +e
-    ps cax |grep fswatch > /dev/null
-    if [ $? -eq 0 ]; then
+    pid_file="${log_path}$(get_project).pid"
+    fswatch_pid=$(cat "${pid_file}" 2> /dev/null)
+    if ps -p "${fswatch_pid}" &> /dev/null; then
         e_success "fswatch è attivo"
     else
         e_error "fswatch non è attivo"
@@ -186,13 +197,35 @@ check() {
     set -e
 }
 
-rewatch() {
-    kill_fswatch
-    e_warning "Rilancio fswatch"
-    $(eval $FSWATCH_CMD) &
-    e_success "Fswatch rilanciato"
+killall_fswatch() {
+    set +e
+    # local status=$?
+    # if $(exit status); then
+    if pgrep fswatch > /dev/null; then
+        kill $(pgrep fswatch |awk '{print $1}')
+    fi
+    set -e
 }
 
+watch() {
+    local logfile
+    local pidfile
+    local pid
+    logfile="${log_path}$(get_project).log"
+    pidfile="${log_path}$(get_project).pid"
+    e_warning "Inizializzo fswatch: log su ${logfile}"
+
+    if eval "${fswatch_cmd} &" ; then
+        #pid=$!
+        pid=$(jobs -p)
+        e_warning "PID: ${pid}"
+        touch "${pidfile}"
+        echo "${pid}" > "${pidfile}"
+        e_success "Fswatch lanciato"
+    else
+        e_error "Problemi con fswatch"
+    fi
+}
 
 stopwatch() {
     e_warning "Killo fswatch"
@@ -201,82 +234,91 @@ stopwatch() {
 }
 
 logwatch() {
+    local logfile
+    logfile="${log_path}$(get_project).log"
     e_header "Log di fswatch"
-    tail -f ${LOG_PATH}
+    tail -f "${logfile}"
 }
 
 update() {
-    local SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
-    local SCRIPTNAME=$(basename "$0")
+    local scriptpath
+    local scriptname
+    local random_string
+    scriptpath=$( cd "$(dirname "$0")" ; pwd -P )
+    scriptname=$(basename "$0")
     e_header "Procedo con l'aggiornamento dello script"
-    e_arrow "Salvo in ${SCRIPTPATH}/${SCRIPTNAME}"
-    curl -fsSL ${SCRIPT_URL}?ran=$(date +"%s") -o ${SCRIPTPATH}/${SCRIPTNAME}
-    if [ $? -eq 0 ]; then
+    e_arrow "Salvo in ${scriptpath}/${scriptname}"
+    random_string=$(date +"%s")
+    if curl -fsSL "${script_url}"?ran="${random_string}" -o "${scriptpath}/${scriptname}"; then
         e_success "Aggiornamento riuscito!"
     else
         e_error "Problemi nell'aggiornamento"
     fi
+    # TODO: creare file rsync
 }
 
 usage() {
-    e_header "Utilizzo $(basename $0)"
-    e_arrow "start:\t\t avvia la macchina e fswatch se non viene passata l'opzione --nowatch"
-    e_arrow "stop:\t\t\t stoppa fswatch e la macchina"
-    e_arrow "check:\t\t controlla lo stato della macchina e di fswatch"
-    e_arrow "rewatch:\t\t rilancia fswatch"
-    e_arrow "stopwatch:\t\t killa fswatch"
-    e_arrow "log:\t\t\t controlla i log di fswatch"
-    e_arrow "update:\t\t aggiorna lo script"
-    e_arrow "test-notification:\t testa le notifiche alle ${REMINDER_TIME}"
-    e_arrow "dc <cmd>:\t\t esegue il comando 'docker-compose <cmd>' sulla macchina di sviluppo"
-    printf "\n"
-    e_warning "per debug esegui: ELECTRO_DEBUG=1 electro <cmd>"
-    printf "\n"
+    e_header "Utilizzo electro:"
+    e_arrow "start:       avvia la macchina. con --nowatch evita di lanciare il watch"
+    e_arrow "stop:        stoppa fswatch e la macchina"
+    e_arrow "check:       controlla lo stato della macchina e di fswatch"
+    e_arrow "watch:       lancia fswatch"
+    e_arrow "stopwatch:   killa fswatch"
+    e_arrow "log:         controlla i log di fswatch"
+    e_arrow "update:      aggiorna lo script"
+    e_arrow "dc <cmd>:    esegue il comando 'docker-compose <cmd>' sulla macchina di sviluppo"
+    e_arrow "test-notification:   testa le notifiche alle ${REMINDER_TIME}"
+    printf "\\n"
+    e_warning "Ricordati di eseguire i comandi nella cartella del progetto sul quali vuoi lavorare!"
+    printf "\\n"
 }
 
 doc_exec() {
-    ssh ${SSH_HOST} "cd ${REMOTE_PATH}/$(basename $DIR_PROJECT);docker-compose "${@:2}""
+    local parameters
+    parameters="${@:2}"
+    ssh "${SSH_HOST}" "cd ${REMOTE_PATH}/$(get_project);docker-compose ${parameters}"
 }
 
 main() {
     local cmd=${1:-}
+    local logfile
 
-    if [ ! -f ${CONFIG_PATH}${CONFIG_FILE} ]; then
-        e_warning "Creo file di configurazione in ${CONFIG_PATH}"
-        mkdir -p ${CONFIG_PATH}
-        touch ${CONFIG_PATH}${CONFIG_FILE}
+    if [ ! -f "${config_path}${config_file}" ]; then
+        e_warning "Creo file di configurazione in ${config_path}"
+        mkdir -p ${config_path}
+        touch ${config_path}${config_file}
         echo 'ELECTRO_INSTANCE_ID="i-xxx"
 HOST="prima"
 SSH_HOST="dev-prima"
-DIR_PROJECT=~/Documents/prima
-' > ${CONFIG_PATH}${CONFIG_FILE}
+' > ${config_path}${config_file}
 
         e_warning "Ricordati di configurare il file!"
         exit
     fi
-    #if [ -z ${ELECTRO_INSTANCE_ID+x} ]; then
-    #    printf "Per favore, configura la variabile ELECTRO_INSTANCE_ID\n"
-    #    exit
-    #fi
 
     if [[ -z "$cmd" ]]; then
         usage
         exit 1
     fi
 
-    source ${CONFIG_PATH}${CONFIG_FILE}
+    if [ ! -d "${log_path}" ]; then
+        mkdir "${log_path}"
+    fi
 
-    INSTANCE_STATUS_CMD="aws ec2 describe-instance-status --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep SYSTEMSTATUS | awk '{print \$2}'"
-    FSWATCH_CMD="fswatch -o ${DIR_PROJECT} | xargs -n1 -I{} rsync -aruzvq --delete --log-file=${LOG_PATH} --exclude=.git/ --exclude=vendor/ --exclude=node_modules/ --exclude=web/assets/ --exclude=web/bower/ --exclude=web/compass/ --exclude=web/bundles/ --exclude=web/webpack/ --exclude=var/cache/ --exclude=var/logs/ --exclude=var/sessions/ --exclude=elm-stuff/ --exclude=app/data/ ${DIR_PROJECT} ${SSH_HOST}:${REMOTE_PATH}"
-    INSTANCE_DESCRIBE_STATUS_CMD="aws ec2 describe-instances --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep -w STATE |awk '{print \$3}'"
+    source "${config_path}${config_file}"
+
+    logfile="${log_path}$(get_project).log"
+    instance_status_cmd="aws ec2 describe-instance-status --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep SYSTEMSTATUS | awk '{print \$2}'"
+    fswatch_cmd="fswatch -o $(pwd) | xargs -n1 -I{} rsync -aruzvq --delete --log-file=${logfile} ${rsync_options} $(pwd) ${SSH_HOST}:${REMOTE_PATH}"
+    instance_describe_status_cmd="aws ec2 describe-instances --instance-ids ${ELECTRO_INSTANCE_ID} --output text |grep -w STATE |awk '{print \$3}'"
     if [[ $cmd == "start" ]]; then
         start_services "$@"
     elif [[ $cmd == "stop" ]]; then
         stop_services
     elif [[ $cmd == "check" ]]; then
         check
-    elif [[ $cmd == "rewatch" ]]; then
-        rewatch
+    elif [[ $cmd == "watch" ]]; then
+        watch
     elif [[ $cmd == "stopwatch" ]]; then
         stopwatch
     elif [[ $cmd == "log" ]]; then
